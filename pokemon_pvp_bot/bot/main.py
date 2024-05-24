fb06eb0301445fcb4c4e8af494f144ed510b441f
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-24 17:23:26 +0530

    Add pokemon_pvp_bot/bot/main.py

---
+++ pokemon_pvp_bot/bot/main.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+bot.run('YOUR_DISCORD_TOKEN')
